b85fc86c7d948cdc13b131755ca86a60d464e1ab
JonatanHjelm95 <cph-jh409@cphbusiness.dk>
2020-05-20 15:57:19 +0200

    wqe

---
+++ build/static/precache-manifest.64776b1dea7e3c658ee2e618a6323b5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7245bb09f7419addc88f2c192fb97b2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59dd5a0d7a4e355ae3c5",
+    "url": "/static/css/main.cd257c2f.chunk.css"
+  },
+  {
+    "revision": "b40156481a32827b8642",
+    "url": "/static/js/2.683af813.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.683af813.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59dd5a0d7a4e355ae3c5",
+    "url": "/static/js/main.04ff67da.chunk.js"
+  },
+  {
+    "revision": "1ee7c313b91f5c4c7a88",
+    "url": "/static/js/runtime-main.8b431099.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
